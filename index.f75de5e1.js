02732b8bb467dbef38b335e8a26e8da84b22c74d
Melnyk Anna <anetamelnyk25@gmail.com>
2024-07-23 15:25:30 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,29 @@
+"use strict";
+function waitFor(element, eventName) {
+    const promise = new Promise((resolve)=>{
+        element.addEventListener(eventName, ()=>{
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+    return promise;
+}
+const printMessage = (message)=>{
+    const body = document.querySelector("body");
+    const elDOM = document.createElement("div");
+    elDOM.setAttribute("class", "message");
+    elDOM.innerText = message;
+    body.appendChild(elDOM);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
